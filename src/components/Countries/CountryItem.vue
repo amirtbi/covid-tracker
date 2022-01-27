a400d12968b9676ad4ddcf9f920887d06ceffcdf
amirtbi <torabi46@gmail.com>
2022-01-27 21:29:28 +0330

    create countries component

---
+++ src/components/Countries/CountryItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <li class="p-4 flex flex-row justify-between align-items-center">
+    <p class="country-title">{{ title }}</p>
+    <p class="country-statics">{{totalDeath}}</p>
+  </li>
+  <!-- <li class="p-4 flex flex-row justify-between align-items-center">
+        <p class="country-title">Iran</p>
+        <p class="country-statics">19990</p>
+      </li> -->
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required:false
+    },
+    totalDeath:{
+        type:String,
+        required:false
+    }
+  },
+  data() {
+    return {};
+  },
+};
+</script>
