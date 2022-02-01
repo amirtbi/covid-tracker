f810996d17d61aad895979b02861a218ea0780e8
amirtbi <torabi46@gmail.com>
2022-02-01 20:29:42 +0330

    some modifications on styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <main class="bg-gray flex flex-row w-full">
     <section class="countries_reports m-4 max-w-4xl">
-      <base-card class="w-full">
-        <article>
+      <base-card class="rounded-2xl w-2">
+        <article class=" bg-primary">
           <div
-            class="search-box p-4 flex flex-col content-center justify-center"
+            class="search-box p-4 flex flex-col  content-center justify-center"
           >
             <form class="w-full form-control">
               <div class="country-box flex flex-col">
                 <label
-                  class="text-purple font-bold font-Roboto mb-2 pl-2"
+                  class="text-primary-200 font-bold font-Roboto mb-4 pl-2"
                   for="country"
-                  >Countries Reports</label
+                  >Cases by Countries</label
                 >
                 <!-- Search box -->
                 <base-search></base-search>
@@ -36,23 +36,23 @@
     </section>
     <!-- section Statics -->
     <section class="ml-6 max-w-5xl mt-4">
-      <base-card class="w-100 rounded-2xl">
+      <base-card class="bg-primary w-100 rounded-2xl">
         <div class="flex flex-col items-start justify-center">
           <header
             class="header p-6 pb-0 w-full flex flex-row items-center justify-center"
           >
-            <h2 class="text-xl text-primary font-Roboto font-medium">
+            <h2 class="text-xl text-primary-200 font-Roboto font-medium">
               Coronavirus Cases
             </h2>
-            <p class="font-medium self-end text-darkblue ml-2 font-Roboto">
+            <p class="font-medium self-end text-primary-200 ml-2 font-Roboto">
               -Worldwide
             </p>
           </header>
           <div class="header_totalConfirmed m-0 p-6">
-            <h4 class="font-medium tracking-wide m-0 font-Roboto text-darkblue">
+            <h4 class="font-medium tracking-wide m-0 font-Roboto text-primary-200">
               Total Confirmed Cases
             </h4>
-            <p class="text-2xl mt-2 text-primary font-bold font-Roboto">
+            <p class="text-2xl mt-2 text-primary-200 font-bold font-Roboto">
               {{ totalConfirmed.toLocaleString() }}
             </p>
           </div>
@@ -62,7 +62,7 @@
           >
             <div
               :style="{ width: newConfirmedWidth + '%' }"
-              class="bg-primary mr-1 h-1 p-2 indicator indicator_active--case"
+              class="bg-tomato mr-1 h-1 p-2 indicator indicator_active--case"
             ></div>
             <div
               :style="{ width: totalDeathsWidth + '%' }"
@@ -70,7 +70,7 @@
             ></div>
             <div
               :style="{ width: newDeathsWidth + '%' }"
-              class="bg-red h-1 p-2 indicator indicator_newDeaths--case"
+              class="bg-yellow h-1 p-2 indicator indicator_newDeaths--case"
             ></div>
           </div>
           <!--Labels -->
@@ -100,9 +100,9 @@ export default {
   data() {
     return {
       categories: [
-        { title: "NewConfirmed", amount: "" },
-        { title: "TotalDeaths", amount: "" },
-        { title: "NewDeaths", amount: "" },
+        { title: "NewConfirmed" },
+        { title: "TotalDeaths" },
+        { title: "NewDeaths" },
       ],
     };
   },
@@ -156,32 +156,13 @@ export default {
       console.log("totalDeaths", this.$store.getters.totalDeaths);
     },
 
-    setCategories() {
-      // NewConfirmed
-      const index = this.categories.findIndex((cat) => {
-        return cat.title === "NewConfirmed";
-      });
-      this.categories[index].amount = this.$store.getters.newConfirmed;
-      
-      // TotalDeaths
-       const indexTD = this.categories.findIndex((cat) => {
-        return cat.title === "TotalDeaths";
-      });
-      this.categories[indexTD].amount = this.$store.getters.totalDeaths;
-      // newDeaths
-       const indexND = this.categories.findIndex((cat) => {
-        return cat.title === "NewDeaths";
-      });
-      this.categories[indexND].amount = this.$store.getters.newDeaths;
-    },
+    
   },
   created() {
     this.loadSummary();
-    this.setCategories();
+  
   },
-  mounted(){
-    this.setCategories();
-  }
+
 };
 </script>
 
@@ -194,23 +175,25 @@ main {
   min-height: calc(100% - 60px);
 }
 div.country-wrapper {
-  /* overflow-y: scroll; */
+  scroll-behavior:smooth;
   max-height: 400px;
+  overflow-x:auto;
+  border-radius: 10px;
 }
-/* div.indicator_container {
-  width: 100%;
-} */
+.country-wrapper::-webkit-scrollbar{
+  width: 10px;
+}
+.country-wrapper::-webkit-scrollbar-thumb{
+ 
+   background-color: #c1c1cc;
+   border-radius: 10px;
+}
+.country-wrapper::-webkit-scrollbar-track{
+  background-color:#9cacbf;
+}
 div.indicator {
   border: none;
   border-radius: 5px;
 }
-/* div.indicator_active--case {
-  width: 20%;
-}
-div.indicator_death--case {
-  width: 40%;
-}
-div.indicator_recovered--case {
-  width: 40%;
-} */
+
 </style>
