07568c0cfeceaed9f98499c0c0323d353c54c1d9
amirtbi <torabi46@gmail.com>
2022-02-04 12:44:36 +0330

    add filtering enteredvalue functionality

--- src/components/UI/baseSearch.vue
+++ src/components/UI/baseSearch.vue
@@ -1,33 +1,62 @@
 <template>
   <div class="container mx-auto flex">
-      <div class="flex bg-darkblue-600 rounded-xl">
-        <button class="flex items-center justify-center px-4">
+    <div class="flex bg-darkblue-600 rounded-xl">
+      <button class="flex items-center justify-center px-4">
         <svg
-            class="text-white w-6 h-6"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            
+          class="text-white w-6 h-6"
+          fill="currentColor"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
         >
-            <path
+          <path
             d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"
-            ></path>
+          ></path>
         </svg>
-        </button>
-        <input type="text" class="bg-darkblue-600 px-4 py-2 w-80 rounded-xl" placeholder="Search..." />
-      </div>
+      </button>
+      <form @submit.prevent="search">
+        <input
+          type="text"
+          @change="search"
+          v-model.trim="countryValue"
+          class="bg-darkblue-600 px-4 py-2 w-80 rounded-xl"
+          placeholder="Search..."
+        />
+      </form>
+    </div>
   </div>
 </template>
 
+<script>
 
+export default {
+  emits:['search'],
+  data() {
+    return {
+      countryValue: "",
+    };
+  },
+
+  methods: {
+    search(event) {
+      // Capitalize entered Value
+      const capitalizedCountry = this.countryValue.split(" ").map((word)=>{
+        return word[0].toUpperCase() + word.slice(1)
+      }).join("");
+
+      this.$emit('search',capitalizedCountry);
+      event.target.value = '';
+    },
+  },
+};
+</script>
 <style scoped>
 
-input:focus{
+input:focus {
   border: none;
   outline: none;
   
 }
-input::placeholder{
+input::placeholder {
   color: white;
   font-family: sans-serif;
 }
