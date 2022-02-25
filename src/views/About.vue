3a87cf6ab59e67f5c6048fe11df45bb54f68886e
amir <torabi46@gmail.com>
2022-02-25 17:39:49 +0330

    update some chart styles

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,22 @@
 <template>
   <section>
     <div class="small">
-      <line-chart :chartData="testData" :options="options"></line-chart>
+      <header class="header-container">
+        <div>
+          <h1>Covid-19 statics</h1>
+          <p>year:{{ chartYearTitle }}</p>
+        </div>
+        <div class="actions">
+          <button>Day</button>
+          <button>Year</button>
+          <button>Month</button>
+        </div>
+      </header>
+      <line-chart
+        id="canvas"
+        :chartData="testData"
+        :options="options"
+      ></line-chart>
     </div>
     <div>
       <ul>
@@ -18,8 +33,7 @@
 import { Chart, registerables } from "chart.js";
 import { LineChart } from "vue-chart-3";
 import { ref, defineComponent, computed } from "vue";
-import { useStore } from "vuex";
-import store from "../store";
+
 //
 Chart.register(...registerables);
 // import lineChart from "../components/Countries/staticChart.vue";
@@ -28,27 +42,41 @@ export default defineComponent({
   components: {
     LineChart,
   },
+  data() {
+    return {
+      monthNames: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ],
+    };
+  },
 
   computed: {
     testData() {
       return {
-        // //labels: ["Paris", "Tehran", "Los Angeles", "Shiraz"],
         labels: this.xLabel,
 
         datasets: [
           {
             data: this.yLabel,
-
-            label: "my custom chart",
+            label: `Switzerland`,
             borderColor: "black",
             fill: true,
-            borderColor: "rgb(75, 192, 192)",
-            borderWidth: 1,
-            backgroundColor: "transparent",
+            borderColor: "rgba(137, 128, 255, 0.8)",
+            borderWidth: 2,
+            backgroundColor: "rgba(245, 155, 255, 0.8)",
             hoverBorderColor: "red",
             pointHoverBackground: "purple",
-            hoverOffset: 4,
-            borderDash: [8, 5],
           },
         ],
       };
@@ -58,7 +86,17 @@ export default defineComponent({
         maintainAspectRatio: false,
         plugins: {
           legend: {
-            display: false,
+            display: true,
+            // Customize the legend
+            labels: {
+              fontFamily: "Comic Sans MS",
+              boxWidth: 60,
+              boxHeight: 20,
+
+              fony: {
+                size: 20,
+              },
+            },
           },
         },
         scales: {
@@ -68,31 +106,37 @@ export default defineComponent({
               borderColor: "black",
             },
             ticks: {
-              maxTicksLimit: 20,
+              // maxTicksLimit: 15,
+              color: "purple",
+              fontStyle: "bold",
+              font: {
+                size: 14,
+              },
             },
           },
           y: {
             grid: {
               borderColor: "black",
-
-              // color: "purple",
+              offest: true,
             },
 
             ticks: {
               beginAtZero: false,
               color: "purple",
-
-              maxTicksLimit: 10,
+              maxTicksLimit: 12,
+              fontStyle: "bold",
               stepSize: Math.ceil(2000 / 5),
-              max: 3600,
+              max: Math.max(...this.yLabel),
+              font: {
+                size: 14,
+              },
             },
           },
         },
         elements: {
           Line: {
-            tension: 0.4,
-            backgroundColor: "yellow",
-            borderColor: "red",
+            tension: 0.1,
+
             borderWidth: 10,
           },
           point: {
@@ -110,21 +154,26 @@ export default defineComponent({
       return this.$store.getters.countryStatic;
     },
     xLabel() {
-      return store.getters.countryStatic.map((data) => {
-        return data.date;
+      return this.$store.getters.countryStatic.map((data) => {
+        return data.Month;
       });
     },
     yLabel() {
-      return store.getters.countryStatic.map((data) => {
+      return this.$store.getters.countryStatic.map((data) => {
         return data.total;
       });
+    },
+    chartYearTitle() {
+      const dates = this.$store.getters.countryStatic.map((data) => {
+        return data.Year;
+      });
+      return dates[0];
     },
   },
   methods: {
     async loadData() {
       try {
         await this.$store.dispatch("addCountryData");
-        console.log(this.xLabel);
       } catch (error) {
         console.log(error.message);
       }
@@ -138,7 +187,44 @@ export default defineComponent({
 
 <style scoped>
 .small {
-  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  max-width: 800px;
   margin: 150px auto;
+  position: relative;
+  box-shadow: 7px 10px 18px -3px rgba(0, 0, 0, 0.1);
+  padding: 2rem;
+}
+.header-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-content: center;
+  width: 100%;
+  margin-bottom: 1rem;
+}
+.actions {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+.header-container h1 {
+  font-family: sans-serif;
+  font-weight: bold;
+  font-size: 2rem;
+}
+button {
+  padding: 0.2rem 1rem;
+  background-color: rgb(223, 217, 217);
+  margin-right: 0.2rem;
+  font-size: 0.5rem;
+}
+button:hover {
+  background-color: gray;
+}
+#canvas {
+  width: 100%;
 }
 </style>
