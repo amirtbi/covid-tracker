6dfa5a93844c3804220b415f35267b655dbe16cb
amir <torabi46@gmail.com>
2022-03-21 21:11:08 +0330

    create countryList component

---
+++ src/components/Countries/country.vue
@@ -0,0 +1,18 @@
+<template>
+  <li
+    class="text-ellipsis text-sm font-bold mb-4 flex flex-row items-center justify-start"
+  >
+    <p class="text-blue-color mr-4 text-ellipsis whitespace-nowrap truncate">
+      {{ countryName }}
+    </p>
+    <h3 class="text-sm text-gray-color">
+      {{ deathAmount.toLocaleString() }}
+    </h3>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["countryName", "deathAmount"],
+};
+</script>
